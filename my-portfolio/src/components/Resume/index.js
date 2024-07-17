70d2258cd297ffa78bab75fd55f83ca977c9d0fb
hach54 <20gunjanarora@gmail.com>
2024-07-17 07:34:06 +0000

    Completed

---
+++ my-portfolio/src/components/Resume/index.js
@@ -0,0 +1,177 @@
+import BarGraph from "./BarGraph";
+import DownloadIcon from "@mui/icons-material/Download";
+
+export default function Resume() {
+  return (
+    <div id="resume" className="min-h-screen flex-col text-center">
+      <div className="text-5xl font-bold py-8" data-aos={"slide-down"}>
+        Resume
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-7 mt-7">
+        <ol class="relative border-l  border-gray-700 ml-9 mb-6">
+          <li class="mb-10" data-aos="zoom-in">
+            <span class="absolute flex items-center justify-center w-6 h-6   rounded-full -left-3 ring-8  ring-gray-900 bg-blue-900">
+              <svg
+                aria-hidden="true"
+                class="w-3 h-3  text-blue-300"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </span>
+            <h3 class="flex items-center mb-1 text-lg font-semibold  text-white ml-28">
+              React Frontend Developer
+            </h3>
+            <time class="block pt-1 pb-3  text-sm font-normal leading-none  text-gray-500">
+              Completed on July 11th, 2024
+            </time>
+            <p class="mb-4 text-base font-normal  text-gray-400">
+            The certificate validates proficiency in React.js, CSS, and JavaScript, covering core React concepts and state management. It also includes backend development with Node.js and Express, and the creation of various mini-projects.
+            </p>
+            <a
+              href="https://www.hackerrank.com/certificates/1cd15de5ff41"
+              target="_blank"
+              rel="noreferrer"
+              class="inline-flex items-center px-4 py-2 mt-2 text-sm font-medium border rounded-lg  focus:z-10 focus:ring-4 focus:outline-none focus:text-blue-700 bg-gray-800 text-gray-400 border-gray-600 hover:text-white hover:bg-gray-700 focus:ring-gray-700"
+            >
+              <svg
+                class="w-4 h-4 mr-2"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M6 2a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V7.414A2 2 0 0015.414 6L12 2.586A2 2 0 0010.586 2H6zm5 6a1 1 0 10-2 0v3.586l-1.293-1.293a1 1 0 10-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 11.586V8z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>{" "}
+              Download Certificate
+            </a>
+          </li>
+          <li class="mb-10" data-aos="zoom-out" data-aos-delay="700">
+            <span class="absolute flex items-center justify-center w-6 h-6 rounded-full -left-3 ring-8 ring-gray-900 bg-blue-900">
+              <svg
+                aria-hidden="true"
+                class="w-3 h-3 text-blue-300"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </span>
+            <h3 class="mb-1 text-lg font-semibold text-white">
+              Generative AI by Google
+            </h3>
+            <time class="block mb-2 text-sm font-normal leading-none text-gray-500">
+              Completed on Febraury 14th, 2024
+            </time>
+            <p class="text-base font-normal text-gray-400">
+            Learned how to build generative AI applications with Python and TensorFlow. I also developed AI models for various tasks and created several mini-projects.
+            </p>
+            <a
+              href="https://www.cloudskillsboost.google/public_profiles/f23dded4-ad03-4b42-842c-b10f4de2862d/badges/8031342"
+              target="_blank"
+              rel="noreferrer"
+              class="inline-flex items-center px-4 py-2 mt-5 text-sm font-medium border rounded-lg  focus:z-10 focus:ring-4 focus:outline-none focus:text-blue-700 bg-gray-800 text-gray-400 border-gray-600 hover:text-white hover:bg-gray-700 focus:ring-gray-700"
+            >
+              <svg
+                class="w-4 h-4 mr-2"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M6 2a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V7.414A2 2 0 0015.414 6L12 2.586A2 2 0 0010.586 2H6zm5 6a1 1 0 10-2 0v3.586l-1.293-1.293a1 1 0 10-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 11.586V8z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>{" "}
+              Download Certificate
+            </a>
+          </li>
+          <li data-aos="zoom-in" data-aos-offset={"-15"} data-aos-delay="1300">
+            <span class="absolute flex items-center justify-center w-6 h-6 rounded-full -left-3 ring-8 ring-gray-900 bg-blue-900">
+              <svg
+                aria-hidden="true"
+                class="w-3 h-3 text-blue-300"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </span>
+            <h3 class="mb-1 text-lg font-semibold text-white">
+              Python Libraries
+            </h3>
+            <time class="block mb-2 text-sm font-normal leading-none text-gray-500">
+              Completed on July 12, 2024
+            </time>
+            <p class="text-base font-normal text-gray-400">
+            Learned how to work with popular Python libraries such as Pandas, NumPy, and Matplotlib. I also explored data manipulation with Pandas and performed data visualization with Matplotlib. Additionally, I built various data analysis projects to understand the practical applications of these libraries.
+            </p>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.hackerrank.com/certificates/5da32f7b75e6"
+              class="inline-flex items-center px-4 py-2 mt-5 text-sm font-medium border rounded-lg focus:z-10 focus:ring-4 focus:outline-none focus:text-blue-700 bg-gray-800 text-gray-400 border-gray-600 hover:text-white hover:bg-gray-700 focus:ring-gray-700"
+            >
+              <svg
+                class="w-4 h-4 mr-2"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M6 2a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V7.414A2 2 0 0015.414 6L12 2.586A2 2 0 0010.586 2H6zm5 6a1 1 0 10-2 0v3.586l-1.293-1.293a1 1 0 10-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 11.586V8z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>{" "}
+              Download Certificate
+            </a>
+          </li>
+        </ol>
+
+        <div className="flex flex-col text-center" data-aos="slide-up">
+          <div className="text-3xl font-bold py-3">My Skills</div>
+          <div className="ml-16 flex flex-col gap-8 mt-6">
+            <BarGraph name={"JavaScript"} percent={65} />
+            <BarGraph name={"React"} percent={75} />
+            <BarGraph name={"Next"} percent={70} />
+            <BarGraph name={"User-Interface Design"} percent={60} />
+            <BarGraph name={"Matplotlib"} percent={80} />
+            <BarGraph name={"HTML/CSS"} percent={85} />
+            <BarGraph name={"SQL"} percent={65} />
+            <BarGraph name={"FireBase"} percent={40} />
+            <BarGraph name={"Django"} percent={30} />
+          </div>
+
+          <a
+            href={require("../../assets/files/Resume.pdf")}
+            download={"Gunjan's Resume"}
+            class="inline-flex items-center px-6 py-2 mt-12 mx-auto w-fit text-sm font-medium border rounded-lg  focus:z-10 focus:ring-4 focus:outline-none focus:text-blue-700 bg-gray-800 text-gray-400 border-gray-600 hover:text-white hover:bg-gray-700 focus:ring-gray-700"
+          >
+            Download CV <DownloadIcon />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ my-portfolio/src/constants/index.js
@@ -0,0 +1,201 @@
+export const projects = [
+  {
+    title: "Mood-Mingle",
+    desc: "Currently Working on this.. MoodMingle, a web application utilizing React.js and AI-powered emotion recognition, designed to connect users with similar emotional states. The app offers a supportive community for mental health discussions, featuring an intuitive interface and seamless integration of emotion detection for a personalized experience.",
+    tags: [
+      {
+        name: "ReactJS",
+        color: "red-600",
+      },
+      {
+        name: "Artificial-Intelligence",
+        color: "green-600",
+      },
+      {
+        name: "TensorFlow",
+        color: "blue-600",
+      },
+      {
+        name: "MongoDB",
+        color: "yellow-600",
+      },
+    ],
+    img: "gotosite.webp",
+  },
+  {
+    title: "Event-Planner",
+    desc: "Developed an event planner application using React.js, featuring a professional design and intuitive user interface. The app includes functionalities such as event creation, user registration, calendar integration, and notifications. Enhanced with Tailwind CSS for stylish visuals and seamless responsiveness, it ensures an engaging and efficient event planning experience across all devices.",
+    tags: [
+      {
+        name: "ReactJS",
+        color: "red-600",
+      },
+      {
+        name: "TailwindCSS",
+        color: "green-600",
+      },
+    ],
+    img: "event-planner.webp",
+    link: "https://pvz-2-api.vercel.app/",
+    code: "https://github.com/code-cracked/plants-vs-zombies-api",
+  },
+  {
+    title: "Photo-Gallery",
+    desc: "A web-based photo gallery application that allows users to upload, manage, and share their favorite photos. This project showcases my skills in [list specific skills, e.g. HTML, CSS, JavaScript, React, Ui Automation, etc.]. The application features a user-friendly interface, responsive design, and robust functionality for uploading, editing, and deleting photos.",
+    tags: [
+      {
+        name: "NextJs",
+        color: "red-600",
+      },
+      {
+        name: "Materialistic-UI",
+        color: "green-600",
+      },
+      {
+        name: "ReactJS",
+        color: "yellow-600",
+      },
+      {
+        name: "MongoDB",
+        color: "blue-800",
+      },
+    ],
+    img: "Photos.webp",
+    link: "https://web-chat-app-brown.vercel.app",
+    code: "https://github.com/gun03-hub/web-chat-app",
+  },
+  {
+    title: "Code-Snippet-Manager",
+    desc: "Developed a robust Code Snippet Manager application using React.js and Node.js, with an integrated front-end interface built with HTML, CSS, and JavaScript. The application allows users to efficiently manage their code snippets with functionalities to add, update, and delete snippets. This project showcases my proficiency in full-stack development, including dynamic front-end design and efficient back-end operations. ",
+    tags: [
+      {
+        name: "Python",
+        color: "red-600",
+      },
+      {
+        name: "ReactJs",
+        color: "green-600",
+      },
+      {
+        name: "NextJS",
+        color: "yellow-600",
+      },
+      {
+        name: "FireBase",
+        color: "blue-800",
+      },
+    ],
+    img: "Code-Snippet-Manager.webp",
+    link: "https://drive.google.com/file/d/1npEOuOhQh-1zRiLB9fFn-6ORTwd1gZkl/view",
+    code: "https://github.com/gun03-hub/EXPLOITER",
+  },
+  {
+    title: "Stock Market",
+    desc: "Stock Market Website done with ReactJs as Frontend and Express as Backend. This websites gives practice for people those who want to start using stock market. It is based on realtime update of stock value.",
+    tags: [
+      {
+        name: "ReactJs",
+        color: "red-600",
+      },
+      {
+        name: "ExpressJs",
+        color: "green-600",
+      },
+      {
+        name: "MongoDB",
+        color: "blue-800",
+      },
+    ],
+    img: "stockmarket.webp",
+    link: "https://stock-market-seven.vercel.app/",
+    code: "https://github.com/gun03-hub/Stock-Market",
+  },
+  {
+    title: "Cash-Calculator",
+    desc: "Developed a Cash Calculator application using React.js, offering an intuitive interface for efficient cash transactions and financial calculations. With support for multiple currencies and detailed summaries, it ensures a seamless user experience across all devices.",
+    tags: [
+      {
+        name: "NextJs",
+        color: "red-600",
+      },
+      {
+        name: "MaterialUI",
+        color: "green-600",
+      },
+      {
+        name: "ExpressJs",
+        color: "yellow-600",
+      },
+      {
+        name: "FireBase",
+        color: "blue-800",
+      },
+    ],
+    img: "Cash-Calculator.webp",
+    link: "https://github.com/code-cracked/you-farm",
+    code: "https://github.com/code-cracked/you-farm",
+  },
+  {
+    title: "Poll-System",
+    desc: "Developed a Poll System using HTML, CSS, and JavaScript, featuring an intuitive interface for creating and participating in polls. The app supports real-time result updates and dynamic voting, providing a seamless and interactive experience across all devices.",
+    tags: [
+      {
+        name: "JavaScript",
+        color: "red-600",
+      },
+      {
+        name: "HTML",
+        color: "green-600",
+      },
+      {
+        name: "CSS",
+        color: "yellow-600",
+      },
+    ],
+    img: "Poll.png",
+    link: "https://github.com/code-cracked/you-farm",
+    code: "https://github.com/code-cracked/you-farm",
+  },
+  {
+    title: "Coin-Flipping",
+    desc: "Developed a Coin Flipping Game using React.js, featuring a simple and engaging interface for random coin flips. The app provides instant results and keeps track of flips, offering an enjoyable experience with responsive design across all devices.",
+    tags: [
+      {
+        name: "JavaScript",
+        color: "red-600",
+      },
+      {
+        name: "HTML",
+        color: "green-600",
+      },
+      {
+        name: "CSS",
+        color: "yellow-600",
+      },
+    ],
+    img: "Coin-Flipping-Game.webp",
+    link: "https://github.com/code-cracked/you-farm",
+    code: "https://github.com/code-cracked/you-farm",
+  },
+  {
+    title: "Stop-Watch",
+    desc: "Developed a Stopwatch application using HTML, CSS, and JavaScript, featuring a sleek and user-friendly interface for precise timekeeping. The app includes start, stop, and reset functionalities, ensuring an accurate and responsive experience across all devices.",
+    tags: [
+      {
+        name: "JavaScript",
+        color: "red-600",
+      },
+      {
+        name: "HTML",
+        color: "green-600",
+      },
+      {
+        name: "CSS",
+        color: "yellow-600",
+      },
+    ],
+    img: "StopWatch.webp",
+    link: "https://github.com/code-cracked/you-farm",
+    code: "https://github.com/code-cracked/you-farm",
+  },
+];
